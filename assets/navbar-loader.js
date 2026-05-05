2637a9c0d62ff946f7bbd3037ea40686d98743f7
manucoacci <manucoacci@gmail.com>
2026-05-05 12:24:39 +0200

    prova fix safari merda

--- assets/navbar-loader.js
+++ assets/navbar-loader.js
@@ -1,6 +1,57 @@
 (function () {
   var NAVBAR_COMPONENT_PATH = "./components/navbar.html";
 
+  function isIOSSafari() {
+    var ua = navigator.userAgent || "";
+    var isIOSDevice =
+      /iPad|iPhone|iPod/.test(ua) ||
+      (ua.indexOf("Macintosh") !== -1 && "ontouchend" in document);
+    var isSafari =
+      ua.indexOf("Safari") !== -1 &&
+      ua.indexOf("CriOS") === -1 &&
+      ua.indexOf("FxiOS") === -1 &&
+      ua.indexOf("EdgiOS") === -1 &&
+      ua.indexOf("OPiOS") === -1;
+    return isIOSDevice && isSafari;
+  }
+
+  function getSportIconElements() {
+    var container = document.querySelector(".nav-icona-sport-container");
+    if (!container) return null;
+
+    return {
+      inlineSvg: container.querySelector("svg"),
+      objectEl: container.querySelector("object[data-sport-icon-object='true']"),
+    };
+  }
+
+  function updateSportObjectForScrollState(navbar) {
+    var els = getSportIconElements();
+    if (!els || !els.objectEl) return;
+
+    if (els.objectEl.style.display === "none") return;
+
+    var isScrolled = !!(navbar && navbar.classList.contains("scrolled-nav"));
+    var desiredSrc = isScrolled
+      ? "assets/icons/sport-white.svg"
+      : "assets/icons/sport-blue.svg";
+
+    if (els.objectEl.getAttribute("data") !== desiredSrc) {
+      els.objectEl.setAttribute("data", desiredSrc);
+    }
+  }
+
+  function setupIOSSportIconFallback(navbar) {
+    if (!isIOSSafari()) return;
+
+    var els = getSportIconElements();
+    if (!els || !els.inlineSvg || !els.objectEl) return;
+
+    els.inlineSvg.style.display = "none";
+    els.objectEl.style.display = "block";
+    updateSportObjectForScrollState(navbar);
+  }
+
   function setupScrollBehavior() {
     var navbar = document.querySelector(".navbar-wrapper");
     if (!navbar) return;
@@ -11,6 +62,8 @@
       } else {
         navbar.classList.remove("scrolled-nav");
       }
+
+      updateSportObjectForScrollState(navbar);
     };
 
     window.addEventListener("scroll", onScroll, { passive: true });
@@ -48,6 +101,7 @@
         }
 
         setupScrollBehavior();
+        setupIOSSportIconFallback(document.querySelector(".navbar-wrapper"));
       })
       .catch(function (error) {
         console.error("Errore navbar-loader:", error);
